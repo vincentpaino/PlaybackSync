afbb69d6086f078b56ee79ad5427a93a588112c3
Vincent <vincentpaino11@gmail.com>
2025-01-08 00:58:16 -0600

    added functions from YT vid, will reevaluate using ExpressJS for auth

--- expressjstest.js
+++ expressjstest.js
@@ -7,18 +7,21 @@ const querystring = require('querystring');
 const { generateRandomString } = require('./functions');
 
 
-var client_id = '82528eb71dcd4f9cb0223b53b6a59ab5';
-var redirect_uri = 'http://localhost:8080/callback';
+var client_id = '82528eb71dcd4f9cb0223b53b6a59ab5'; // client_
+var client_secret = "";
+var redirect_uri = 'http://localhost:8080/callback'; //callback url
+const AUTHORIZE = "https://accounts.spotify.com/authorize"
+const TOKEN = "https://accounts.spotify.com/api/token";
 
 var app = express();
 
-//app.get function -- 
+//app.get function -- same as requestAuthorization **Probably NOT using the expressjs way...
 app.get('/login', function(req, res) {
 
-  var state = generateRandomString(16);
-  var scope = 'user-read-private user-read-email'; //Look at what these scopes mean
+  var state = generateRandomString(16); //Optional
+  var scope = 'user-read-private user-read-email'; //Look at what these scopes mean, we're asking the permissions we want, which are the user's read-email, read-private
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
+  res.redirect('https://accounts.spotify.com/authorize?' +  //querystring.stringify() builds the url we are redirecting to (after authorization)
     querystring.stringify({
       response_type: 'code',
       client_id: client_id,
--- functions.js
+++ functions.js
@@ -5,6 +5,68 @@ function generateRandomString(length) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
+}
+
+// Retrieves the code from the URL, which is then used in the 'body' in 
+// fetchAccessToken
+function getCode(){
+  let code = null;
+  const queryString = window.location.search;
+  if(querystring > 0){
+    const urlParams = new URLSearchParams(querystring);
+    code = urlParams.get('code');
   }
+  return code;
+}
+
+// After user accepts request, this function creates the 'body' with its
+// respective parameters, which is then used in callAuthorizationAPI(body)
+function fetchAccessToken(code){
+  let body = "grant_type=authorization_code";
+  body += "&code" + code;
+  body += "&redirect_uri=" + encodeURI(redirect_uri);
+  body += "&client_id=" + client_id;
+  body += "&client_secret=" + client_secret;
+  callAuthorizationAPI(body);
+}
+
+function callAuthorizationAPI(body){
+  let xhr = new XMLHttpRequest();
+  xhr.OPEN("POST", TOKEN, true);
+  xhr.setRequestHeader('Content-Type', application/x-www-form-urlencoded);
+  xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+  xhr.send(body);
+  xhr.onload = handleAuthorizationResponse;
+}
+
+function handleAuthorizationResponse(){
+  if (this.status == 200){
+      var data = JSON.parse(this.responseText);
+      console.log(data);
+      var data = JSON.parse(this.responseText);
+      if(data.access_token != undefined){
+          access_token = data.access_token;
+          localStorage.setItem("access_token", access_token);
+      }
+      if(data.refresh_token  != undefined){
+          refresh_token = data.refresh_token;
+          localStorage.setItem("refresh_token", refresh_token);
+      }
+      onPageLoad();
+  }
+  else {
+      console.log(this.responseText);
+      alert(this.responseText);
+  }
+}
+
+function callAPI(method, url, body, callback){
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, true)
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+  xhr.send(body);
+  xhr.onload = callback;
+}
 
   module.exports = { generateRandomString };
